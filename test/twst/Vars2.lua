b411553019ba711d2f0bf7f735ed6f4fbabeeb25
bikethatreportsbugs36 <caniconnectyoutoabike@gmail.com>
2025-11-04 19:25:51 -0800

    3

--- test/twst/Vars2.lua
+++ test/twst/Vars2.lua
@@ -2,7 +2,7 @@ local goombas = 12
 local koopaTroopas = 5
 local totalEnemies = goombas + koopaTroopas
 print("Total enemies: " .. totalEnemies)
-e = "C"
+local e = "C"
 e = e .. "o"
 e = e .. "d"
 e = e .. "e"
@@ -13,3 +13,47 @@ local sauceType = "Marinara"
 local goombaNames = {"Goomba", "Goombella", "Goombario"}
 print("Sauce Type: " .. sauceType)
 print("First Goomba Name: " .. goombaNames[1])
+local coinsListPerLevel = {160, 220, 360, 405, 44, 763, 643, 234, 123, 543, 654, 321, 6, 54, 6668, 56, 2345, 234, 1234, 5432, 3456}
+local coins = 0
+-- for i = 1, #coinsListPerLevel do
+    -- coins = coins + coinsListPerLevel[i]
+    
+-- end
+print("Total Coins Collected: " .. coins)
+local averageCoins = coins / #coinsListPerLevel
+warn("cannot divide by zero")
+print("Average Coins per Level: " .. averageCoins)
+goombaNames[4] = "Goombette"
+print("New Goomba Name: " .. goombaNames[4])    
+goombaNames[2] = nil
+--error("404: Goombella Not Found")
+print("This line will be executed due to the error above.")
+local TEMPLIST = {4, 5, 6, 7}
+
+local goombaCount = 7
+
+print(goombaNames)
+print(TEMPLIST)
+print(goombaCount)
+print(a)
+sauceType = sauceType .. ", Pesto"
+warn("Updatedce Type: " .. sauceType)
+local allSauceTypes = {"Marinara", "Pesto", "Alfredo", "BBQ", "Ranch", "Salsa", "Garlic", "Buffalo", "Teriyaki", "Sweet and Sour"}
+-- 404: Goombella Not Found
+--- print("All Sauce Types: " .. allSauceTypes)
+-- commented out becuase it causes an error
+print("End of Vars2.lua")
+print("(In computer voice) JUST KIDDING, CONTINUING EXECUTION.")
+local FoodTypes = {"Pizza", "Burger", "Pasta", "Salad", "Sushi", "Taco", "Sandwich", "Soup", "Steak", "Curry", "Dumplings", "Ramen", "Paella", "Kebab", "Falafel", "Gnocchi", "Quiche", "Bruschetta", "Ceviche", "Chowder", "Biryani", "Fajitas", "Goulash", "Hummus", "Jambalaya", "Katsu", "Lobster", "Moussaka", "Naan", "Okonomiyaki", "Poutine", "Quesadilla", "Risotto", "Samosa", "Tapas", "Udon", "Vindaloo", "Wontons", "Xiaolongbao", "Yakisoba", "Ziti", "Donuts", "Eclairs", "Frittata", "Gelato", "Hotpot", "Ice Cream", "Jelly", "Kale Salad", "Lassi", "Macarons", "Nuggets"}
+local YummyMeal = FoodTypes[1] .. ", " .. FoodTypes[10] .. ", and " .. FoodTypes[20] .. "!"
+local eofthing = " "
+print("Today's Yummy Meal includes: " .. YummyMeal)
+local CarType = "Convertible"
+local CarColor = "Red"
+local CarSpeed = 150
+local carDebugInfo1 = {"CarTypeValue = {placeholder}" .. CarType, "CarColorValue = #FF0000 /" .. CarColor, "CarSpeedValueMPH = " .. CarSpeed, "CarSpeedValueKPH = " .. (CarSpeed * 1.60934)}
+print("Car Details: " .. CarColor .. " " .. CarType .. " with a top speed of " .. CarSpeed .. " mph.")
+
+print("End of Vars2.lua execution.")
+local DebugInfo = {"scriptID = 0x00000001", "creator = 'bikethatreportsbugs36'", "version = 1.0.3", "lastModified = '2025-11-04'", "goombas = Goomba, ", "nil - content was Goombella, ", "Goombario, ", "Goombette", "koopaTroopas = 5", "totalEnemies = 17", "sauceType = Marinara, Pesto", "averageCoinsPerLevel = 243.15", "YummyMeal = Pizza, Soup, and Chowder!", "pizzaCount = 1.0000000000000372605629576338125", "k = v", "n = nil "}
+print("erererererer")
