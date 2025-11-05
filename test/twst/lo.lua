28f9fdef66345970573ce79d1c363a5a2466c53e
bikethatreportsbugs36 <caniconnectyoutoabike@gmail.com>
2025-11-04 18:26:26 -0800

    2
    2

--- test/twst/lo.lua
+++ test/twst/lo.lua
@@ -4,3 +4,33 @@ a = a + 10
 print("The new value of a is:", a)
 local b = 42
 print("The value of b is:", b)
+ab = a * 2, b / 2
+if ab > b then
+    print("a is greater than b")
+else
+    print("a is not greater than b")
+end
+
+local c = "Hello, World!"
+print(c)
+local d = 8480
+print("The value of d is:", d)
+d = d - 80
+print("The new value of d is:", d)
+d = d + 20
+print("The updated value of d is:", d)
+if d < 8400 then
+    print("d is less than 8400")
+else
+    print("d is not less than 8400")
+end
+local e = "Lua Scripting"
+print(e)
+e = e .. " is fun!"
+print(e)
+local f = 100
+print("The value of f is:", f)
+f = f / 4
+print("The new value of f is:", f)
+print("Example script completed. Table: a: ", a, "b: ", b, "d: ", d, "f: ", f)
+print("Error codes: 7 (define param num(1) < assign param num(2) error) ")
