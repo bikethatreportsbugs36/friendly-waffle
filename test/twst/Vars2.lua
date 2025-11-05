ad24935ce27ac92ee7537c74712be1159888e556
bikethatreportsbugs36 <caniconnectyoutoabike@gmail.com>
2025-11-04 18:35:05 -0800

    new thinga

---
+++ test/twst/Vars2.lua
@@ -0,0 +1,15 @@
+local goombas = 12
+local koopaTroopas = 5
+local totalEnemies = goombas + koopaTroopas
+print("Total enemies: " .. totalEnemies)
+e = "C"
+e = e .. "o"
+e = e .. "d"
+e = e .. "e"
+print("Constructed string: " .. e)
+
+local a = 48
+local sauceType = "Marinara"
+local goombaNames = {"Goomba", "Goombella", "Goombario"}
+print("Sauce Type: " .. sauceType)
+print("First Goomba Name: " .. goombaNames[1])
